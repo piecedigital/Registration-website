b2d1b0d5d787bf7b7d2ce6541e74743a243569a2
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-07-11 17:13:04 -0400

    fixed temporary favicon

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.set("view options", { layout: "layout" });
 
 // config
 // uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/logo.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
