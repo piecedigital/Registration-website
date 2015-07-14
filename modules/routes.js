f780993f53d8f59352c2cff6e806ddab4d0ebbb5
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-07-14 18:32:47 -0400

    added new fields

--- modules/routes.js
+++ modules/routes.js
@@ -27,12 +27,14 @@ var mailer = require("./mailer");
 					firstName = req.body["first-name"],
 					lastName = req.body["last-name"],
 					fullName = firstName + " " + lastName,
-					set = req.body.set;
+					set = req.body.set
+					tel = req.body.tel
+					church = req.body.church;
 			if(email && firstName && lastName && set) {
 				if( email.match(/([a-z0-9])*([.][a-z0-9]*)?([@][a-z0-9]*[.][a-z]{1,3})([.][a-z]{1,2})?/i) ) {
 					mailer("Confirmation", email, fullName, "Hello, " + firstName + ",\n\r\n\r This email is to confirm that your email has been sent succsessfully.").mailPost();
 
-					mailer("New registrant", "piecedigitalstudios@gmail.com", "maintainer", "Hello, maintainer,\n\r\n\r You have recaived a new submission:\r\n <ul><li>Email: " + email + "</li><li>Name: " + fullName + "</li><li>Set: " + set + "</li></ul>.").mailPost();
+					mailer("New registrant", "piecedigitalstudios@gmail.com", "maintainer", "Hello, maintainer,\n\r\n\r You have recaived a new submission:\r\n <ul><li>Email: " + email + "</li><li>Name: " + fullName + "</li><li>Set: " + set + "</li><li>Phone: " + tel + "</li><li>CHurch Affiliation: " + church + "</li></ul>.").mailPost();
 
 					res.render("register", { msg : "You information has bee submitted for review. An email has been sent to you for confirmation" });
 				} else {
