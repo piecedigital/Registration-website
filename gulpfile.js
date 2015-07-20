a6ab435d62162a5460a41a81093ab058eaf4920e
Darryl Benton Dixon <piecedigitalstudios@gmail.com>
2015-07-20 14:51:57 -0400

    updated footer

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 var gulp = require("gulp");
 var browserSync = require("browser-sync").create();
-var nodemon = require("gulp-nodemon")
+var nodemon = require("gulp-nodemon");
 var reload = browserSync.reload;
+var sass = require('node-sass');
+var fs = require("fs");
+
+function renderSass() {
+	sass.render({
+	  file: "./private/sass/style.scss",
+	  outputStyle: "expanded",
+	  outFile: "./public/css/styl.css"
+	}, function(err, result) {
+		if(err) throw err;
+
+		//console.log(result.css.toString());
+		fs.writeFile('./public/css/style.css', result.css.toString(), function (err) {
+		  if (err) throw err;
+
+		  console.log('CSS rendered and saved');
+		});
+	});
+}
 
 gulp.task("start", function() {
 	nodemon({
@@ -16,7 +35,8 @@ gulp.task("serve", function() {
 		proxy: "http://localhost:8080",
 		port: 8081
 	});
-
+ 
+	gulp.watch(["private/sass/*.scss"]).on("change", renderSass);
 	gulp.watch(["views/*.hbs", "public/css/*.css", "public/js/*.js"]).on("change", reload);
 });
 
